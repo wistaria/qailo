bea91a1c15bb48a10f7ba08a3e2d6f4e01f2c272
SS <hclfuranpon_compotayamano_0117@outlook.jp>
2024-03-04 02:31:56 +0900

    :construction: Annotate state_vector/type.py

--- src/qailo/state_vector/type.py
+++ src/qailo/state_vector/type.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+
+from typing import Any
+
 import numpy as np
+import numpy.typing as npt
+from typing_extensions import TypeGuard
 
 
-def is_state_vector(v):
+def is_state_vector(v: Any) -> TypeGuard[np.ndarray]:
     if isinstance(v, np.ndarray):
         return v.shape[-1] == 1 and v.shape[-2] > 1
     else:
         return False
 
 
-def num_qubits(v):
+def num_qubits(v: npt.NDArray) -> int:
     return v.ndim - 1
