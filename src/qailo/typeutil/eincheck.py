a24b604e23c70a7379bb4b3dc0baebe16e36fb0b
SS <hclfuranpon_compotayamano_0117@outlook.jp>
2024-03-04 04:20:27 +0900

    :safety_vest: Add eincheck.py

---
+++ src/qailo/typeutil/eincheck.py
@@ -0,0 +1,70 @@
+"""Wrap np.einsum for better type checking."""
+
+from __future__ import annotations
+
+from typing import overload
+
+import numpy as np
+import numpy.typing as npt
+
+
+@overload
+def einsum(op1: np.ndarray, ss1: list[int]) -> npt.ArrayLike: ...
+
+
+@overload
+def einsum(op1: np.ndarray, ss1: list[int], ss_out: list[int]) -> npt.ArrayLike: ...
+
+
+@overload
+def einsum(
+    op1: np.ndarray,
+    ss1: list[int],
+    op2: np.ndarray,
+    ss2: list[int],
+) -> npt.ArrayLike: ...
+
+
+@overload
+def einsum(
+    op1: np.ndarray,
+    ss1: list[int],
+    op2: np.ndarray,
+    ss2: list[int],
+    ss_out: list[int],
+) -> npt.ArrayLike: ...
+
+
+def einsum(*args, **kwargs):
+    return np.einsum(*args, **kwargs)
+
+
+@overload
+def einsum_cast(op1: np.ndarray, ss1: list[int]) -> npt.NDArray: ...
+
+
+@overload
+def einsum_cast(op1: np.ndarray, ss1: list[int], ss_out: list[int]) -> npt.NDArray: ...
+
+
+@overload
+def einsum_cast(
+    op1: np.ndarray,
+    ss1: list[int],
+    op2: np.ndarray,
+    ss2: list[int],
+) -> npt.NDArray: ...
+
+
+@overload
+def einsum_cast(
+    op1: np.ndarray,
+    ss1: list[int],
+    op2: np.ndarray,
+    ss2: list[int],
+    ss_out: list[int],
+) -> npt.NDArray: ...
+
+
+def einsum_cast(*args, **kwargs):
+    return np.asarray(np.einsum(*args, **kwargs))
