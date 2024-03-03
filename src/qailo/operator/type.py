aba083887ee84f41dc46e188998d7d16de5dd8e0
SS <hclfuranpon_compotayamano_0117@outlook.jp>
2024-03-04 02:34:23 +0900

    :construction: Annotate operator/type.py

--- src/qailo/operator/type.py
+++ src/qailo/operator/type.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
+
+from typing import Any
+
 import numpy as np
+import numpy.typing as npt
+from typing_extensions import TypeGuard
 
 
-def is_density_matrix(v):
+def is_density_matrix(v: Any) -> TypeGuard[np.ndarray]:
     if isinstance(v, np.ndarray):
         return (
             v.shape[-1] == 1
@@ -12,13 +18,13 @@ def is_density_matrix(v):
     return False
 
 
-def is_operator(v):
+def is_operator(v: Any) -> TypeGuard[np.ndarray]:
     if isinstance(v, np.ndarray):
         return v.shape[-1] > 1 and v.ndim % 2 == 0
     return False
 
 
-def num_qubits(v):
+def num_qubits(v: npt.NDArray) -> int:
     if is_density_matrix(v):
         return (v.ndim - 2) // 2
     elif is_operator(v):
