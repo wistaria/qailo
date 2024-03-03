65ee3c4abba4abd25ebf5a54b96aac0597ec21d1
SS <hclfuranpon_compotayamano_0117@outlook.jp>
2024-03-04 04:17:54 +0900

    :art: Remove inverse slice

--- src/qailo/operator/inverse.py
+++ src/qailo/operator/inverse.py
@@ -4,7 +4,7 @@ from .type import is_operator
 
 def inverse_seq(seq):
     res = []
-    for p, pos in seq[::-1]:
+    for p, pos in reversed(seq):
         assert is_operator(p)
         res.append([hconj(p), pos])
     return res
